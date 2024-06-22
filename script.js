976867bc963048bbbb59b2c62895abb85dfe526e
JosephChirra <josephchirra43@.com>
2024-06-23 00:40:52 +0530

    second commit

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let popupEl=document.getElementById("Popup")
 let closeEl=document.getElementById("close")
 let openEl=document.getElementById("button-1")
 
+
+
 function openpopup(){
     popupEl.style.display="flex"
 }
@@ -11,3 +13,20 @@ function openpopup(){
 function closepopup(){
     popupEl.style.display="none"
 }
+
+
+let popupEl2=document.getElementById("Popup-2")
+
+let close3El=document.getElementById("close-4")
+let open2El=document.getElementById("button-2")
+
+
+
+function openpopup2(){
+    popupEl2.style.display="flex"
+}
+
+function closepopup2(){
+    popupEl2.style.display="none"
+}
+
