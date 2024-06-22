88de3131a1ffb2e1f22f21ae13acc42d736193b3
JosephChirra <josephchirra43@.com>
2024-06-23 00:16:07 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let popupEl=document.getElementById("Popup")
+
+let closeEl=document.getElementById("close")
+let openEl=document.getElementById("button-1")
+
+function openpopup(){
+    popupEl.style.display="flex"
+}
+
+function closepopup(){
+    popupEl.style.display="none"
+}
